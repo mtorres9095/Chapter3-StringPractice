16584ec808cfef72a0b99c96c30e9c6b55a6986b
mtorres9095 <mtorres9095@gmail.com>
2020-06-03 11:28:52 -0400

    Complete the exercises

--- Chapter3 StringPractice/Program.cs
+++ Chapter3 StringPractice/Program.cs
@@ -6,19 +6,16 @@ namespace Chapter3_StringPractice
     {
         static void Main(string[] args)
         {
-            string[] arr1; 
+            //string[] arr1 = new string[32];
             string str = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
-            for (int i = 0; i < str.Length; i++) {
-                arr1[i] = str.Split(" ");
-                Console.WriteLine(arr1[i]);
-                consolde
+            //char splitchar = ' ';
+            string[] arr1 = str.Split(", ");
 
-
-
-
+            for (int i = 0; i < arr1.Length; i++) {
+                         
             }
-
-
+            Console.WriteLine(string.Join(" ", arr1));
+           
 
 
 
